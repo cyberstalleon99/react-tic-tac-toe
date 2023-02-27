3ebee7e0326c0ec8c1d78548eeadb3d7c90d1c9a
Khayad, Clyde (Taguig City) <Clyde.Khayad@wtwco.com>
2023-02-27 22:21:39 +0800

    Exercise 5 done (using object instead of Array): When someone wins, highlight the three squares that caused the win.

--- src/index.js
+++ src/index.js
@@ -175,7 +175,7 @@ class Game extends React.Component {
     let status;
 
     if (winner) {
-      status = 'Winner: ' + winner[0];
+      status = 'Winner: ' + winner.player;
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X': 'O');
     }
@@ -184,7 +184,7 @@ class Game extends React.Component {
       <div className="game">
         <div className="game-board">
           <Board 
-            winnerRow={winner ? winner[1] : null}
+            winnerRow={winner ? winner.row : null}
             squares={current.squares}
             onClick={(i) => this.handleClick(i)}
           />
@@ -224,7 +224,10 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return [squares[a], [a, b, c]];
+      return {
+        player: squares[a], 
+        row: [a, b, c]
+      }
     }
   }
   return null;
